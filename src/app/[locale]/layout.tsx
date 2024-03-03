045b9180639b5e46f30563f1893e972a900040df
donifz <doniattokurovich@gmail.com>
2024-03-03 18:27:56 +0600

    robots

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -17,10 +17,10 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Уют хаус хостел",
     description: "Остановитесь у нас и почувствуйте себя как дома",
-    siteName: "Online Store",
+    siteName: "Uyut house hostel",
     type: "website",
   },
-  keywords:["hostel", "хостел", "house", "osh", "Osh", "Ош", "uyut house osh hostel", "Гостиница", "квартира", "hotel", "guest house"]
+  keywords:["hostel", "хостел", "house", "osh", "Osh", "Ош", "uyut house osh hostel", "Гостиница", "квартира", "hotel", "guest house", "бронирование номеров", "дружелюбный персонал", "уютные номера"],
 };
 
 export default function RootLayout({
@@ -31,6 +31,14 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <meta name="google-site-verification" content="M0w3Yyft_ONe2sw-GrSkFiYt1TrfyM9Pte5G1sUY5xE" />
+      <meta name="author" content="Уют хаус хостел"/>
+      <meta name="robots" content="index, follow"/>
+      <meta name="revisit-after" content="7 days"/>
+      <meta name="distribution" content="global"/>
+      <meta name="rating" content="general"/>
+      <meta name="geo.placename" content="Osh"/>
+      <meta name="geo.region" content="KG"/>
+
       <body className={inter.className}>
         <Layout>
           {children}
